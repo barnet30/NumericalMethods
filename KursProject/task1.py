4ab71e4d9faf40b718c8bc042220386ef966fb31
Павел Гавриличев <PNGavrilichev@stud.kpfu.ru>
2021-04-14 11:21:11 +0300

    upd

--- KursProject/task1.py
+++ KursProject/task1.py
@@ -1,31 +1,52 @@
 import numpy as np
-from math import factorial
 import pandas as pd
 from matplotlib import pyplot as plt
+
 
 a = 0
 b = 3
 h = 0.3
 eps = 1e-6
+MAX_STEPS = 1000
 
 
 #Заменить функцию факториала
+# def taylor(x):
+#     y = 0 # Начало суммы
+#     sign = 1 # Знак первого члена ряда
+#     s = 1 # Первый член ряда
+#     k=0
+#     fact_temp = 1
+#     while abs(s) > eps and k <= MAX_STEPS:
+#         # Прибавляем член к ряду
+#         y += sign*s
+#         # Вычисляем следующий член ряда
+#         k += 1
+#         sign = - sign
+#         fact_temp *= k
+#         denom = fact_temp**2
+#         s = ((x*x/4)**k)/denom
+#
+#     return x, y, k
+
 def taylor(x):
-    y = 0 # Начало суммы
-    sign = 1 # Знак первого члена ряда
-    s = 1 # Первый член ряда
-    k=0
-    while abs(s) > eps:
+    y = 0  # Начало суммы
+    sign = 1  # Знак первого члена ряда
+    s = 1  # Первый член ряда
+    k = 0
+    factorial_temp = 1
+    while abs(s) > eps and k <= MAX_STEPS:
         # Прибавляем член к ряду
-        y += sign*s
+        y += sign * s
         # Вычисляем следующий член ряда
         k += 1
-        sign = - sign
-        denom = (factorial(k))**2
-        s = s * ((x*x/4)**k)/denom
+        sign = sign * (-1)
+        factorial_temp *= k
+        denom = factorial_temp ** 2
+        s = ((x * x / 4) ** k) / denom
 
     return x, y, k
-
+""" Task 1 """
 x_array = []
 f_array = []
 k_array = []
@@ -42,6 +63,9 @@ table = np.array([x_array,f_array,k_array])
 pd.options.display.max_columns = 50
 df = pd.DataFrame(table)
 print(df)
-#
+
 # plt.plot(x_array,f_array)
 # plt.show()
+#
+# plt.plot()
+
--- KursProject/task2.py
+++ KursProject/task2.py
@@ -2,8 +2,13 @@ import numpy as np
 from math import factorial
 import pandas as pd
 from matplotlib import pyplot as plt
-from KursProject.task1 import x_array, f_array
+from KursProject.task1 import x_array,f_array,taylor
 
+a = 0
+b = 3
+n=10
+def make_uniform_partition(a,b,n):
+    return np.linspace(a,b,num=n)
 
 def add(p1,p2):
 
@@ -14,7 +19,7 @@ def add(p1,p2):
     res.sort(key = lambda r: r[1], reverse= True)
     return res
 
-def mul(p1,p2):
+def mult(p1,p2):
 
     x = [0]*(p1[0][1]*p2[0][1]+2)
     for i in p1:
@@ -24,7 +29,7 @@ def mul(p1,p2):
     res.sort(key = lambda r: r[1], reverse= True)
     return res
 
-def mul_numb(p1, numb):
+def mult_numb(p1, numb):
     res = [[i[0] * numb,i[1]] for i in p1]
     return res
 
@@ -38,25 +43,35 @@ def poly_val(p1, value):
         res += x*poly[i][0]
     return res
 
-n = len(x_array)
 
-lagranz = [(0,0),(0,0)]
-poly = list()
+def interpolation_lagranz(x):
+    lagranz = [(0, 0), (0, 0)]
+    n = len(x)
 
-for i in range(n):
-    denom = 1
-    for j in range(n):
-        if i!=j:
-            denom *= (x_array[i] - x_array[j])
-            if j==0 or (i==0 and j==1):
-                poly = [[1,1],[-x_array[j],0]]
-            else:
-                poly = mul(poly,[[1,1],[-x_array[j],0]])
+    poly = list()
 
-    if f_array[i]!=0:
-        coef = f_array[i]/denom
-        poly = mul_numb(poly, coef)
-        lagranz = add(lagranz,poly)
-print(lagranz)
+    for i in range(n):
+        denom = 1
+        for j in range(n):
+            if i != j:
+                denom *= (x[i] - x[j])
+                if j == 0 or (i == 0 and j == 1):
+                    poly = [[1, 1], [-x[j], 0]]
+                else:
+                    poly = mult(poly, [[1, 1], [-x[j], 0]])
 
-print(poly_val(lagranz, x_array[2]),f_array[2])
+        f_ksi = taylor(x[i])
+        if f_ksi[i] != 0:
+            coef = f_ksi[i] / denom
+            poly = mult_numb(poly, coef)
+            lagranz = add(lagranz, poly)
+    return lagranz
+
+errors = []
+for i in range(n,101,20):
+    x_array = make_uniform_partition(a,b,i)
+    lagragnz = interpolation_lagranz(x_array)
+    diff = [abs(taylor(x_value)[0] - poly_val(lagragnz,x_value)) for x_value in x_array]
+    errors.append([n,max(diff)])
+
+print(errors)
